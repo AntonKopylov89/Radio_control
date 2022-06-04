edd048c17e77617826d1c6401520da1ce1021ae9
AntonKopylov89 <ttxb@yandex.ru>
2022-06-04 14:56:14 +0400

    Add selection of the number of radio station

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -8,126 +8,112 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTest {
 
+    Radio rad = new Radio();
     @ParameterizedTest
     @CsvFileSource(resources = "/dataCurrentRadioStation.csv")
     void shouldSetCurrentRadioStation(int currentRadioStation, int expected) {
-        Radio rad = new Radio();
 
         rad.setCurrentRadioStation(currentRadioStation);
 
-        int actual = rad.getCurrentRadioStation();
+        assertEquals(expected, rad.getCurrentRadioStation());
+    }
 
-        assertEquals(expected, actual);
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataNumberOfRadioStation.csv")
+    void shouldSetNumberOfRadioStation(int numberOfRadioStation, int expected) {
+
+        rad.setNumberOfRadioStation(numberOfRadioStation);
+
+        assertEquals(expected, rad.getNumberOfRadioStation());
     }
 
     @Test
     void sholdNextRadioStation() {
-        Radio rad = new Radio();
-
         rad.setCurrentRadioStation(1);
 
         rad.setNext();
 
-        int actual = rad.getCurrentRadioStation();
-        int expected = 2;
-
-        assertEquals(expected, actual);
+        assertEquals(2, rad.getCurrentRadioStation());
     }
 
     @Test
     void sholdNextRadioStationFrom9To0() {
-        Radio rad = new Radio();
-
         rad.setCurrentRadioStation(9);
 
         rad.setNext();
 
-        int actual = rad.getCurrentRadioStation();
-        int expected = 0;
-
-        assertEquals(expected, actual);
+        assertEquals(0, rad.getCurrentRadioStation());
     }
 
     @Test
     void sholdPrevRadioStation() {
-        Radio rad = new Radio();
-
         rad.setCurrentRadioStation(1);
 
         rad.setPrev();
 
-        int actual = rad.getCurrentRadioStation();
-        int expected = 0;
-
-        assertEquals(expected, actual);
+        assertEquals(0, rad.getCurrentRadioStation());
     }
 
     @Test
     void sholdPrevRadioStationFrom0To9() {
-        Radio rad = new Radio();
-
         rad.setCurrentRadioStation(0);
 
         rad.setPrev();
 
-        int actual = rad.getCurrentRadioStation();
-        int expected = 9;
-
-        assertEquals(expected, actual);
+        assertEquals(9, rad.getCurrentRadioStation());
     }
 
     @Test
     void sholdIncreaseVolume() {
-        Radio rad = new Radio();
-
         rad.increaseVolume();
 
-        int actual = rad.getCurrentVolume();
-        int expected = 1;
-
-        assertEquals(expected, actual);
+        assertEquals(1, rad.getCurrentVolume());
     }
 
     @Test
-    void sholdIncreaseVolumeFrom10() {
-        Radio rad = new Radio();
-
-        rad.setCurrentVolume(10);
+    void sholdIncreaseVolumeFrom100() {
+        rad.setCurrentVolume(100);
 
         rad.increaseVolume();
 
-        int actual = rad.getCurrentVolume();
-        int expected = 10;
-
-        assertEquals(expected, actual);
+        assertEquals(100, rad.getCurrentVolume());
     }
 
     @Test
     void sholdReduseVolume() {
-        Radio rad = new Radio();
-
         rad.reduceVolume();
 
-        int actual = rad.getCurrentVolume();
-        int expected = 0;
-
-        assertEquals(expected, actual);
+        assertEquals(0, rad.getCurrentVolume());
     }
 
     @Test
     void sholdReduseVolumeFrom1() {
-        Radio rad = new Radio();
-
         rad.setCurrentVolume(1);
 
         rad.reduceVolume();
 
-
-        int actual = rad.getCurrentVolume();
-        int expected = 0;
-
-        assertEquals(expected, actual);
+        assertEquals(0, rad.getCurrentVolume());
     }
 
 
+
+    Radio rad1 = new Radio(25);
+    @Test
+    void sholdNextRadioStationFrom24To0() {
+
+        rad1.setCurrentRadioStation(24);
+
+        rad1.setNext();
+
+        assertEquals(0, rad1.getCurrentRadioStation());
+    }
+
+    @Test
+    void sholdPrevRadioStationFrom0To24() {
+        rad1.setCurrentRadioStation(0);
+
+        rad1.setPrev();
+
+        assertEquals(24, rad1.getCurrentRadioStation());
+    }
 }
